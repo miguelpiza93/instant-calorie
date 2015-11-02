a4bd718cac3c8a0b9d150c39dd0cfa10b43534d1
leebeth <lorena_2328@hotmail.com>
2015-11-02 15:05:15 -0500

    Camara con preview, toma foto y guarda

---
+++ src/com/calorie/instant/CameraActivity.java
@@ -0,0 +1,90 @@
+package com.calorie.instant;
+
+import java.io.File;
+import static com.calorie.instant.util.CameraHelper.cameraAvailable;
+import static com.calorie.instant.util.CameraHelper.getCameraInstance;
+import static com.calorie.instant.util.MediaHelper.getOutputMediaFile;
+import static com.calorie.instant.util.MediaHelper.saveToFile;
+import android.app.Activity;
+import android.content.Intent;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.os.Bundle;
+import android.view.View;
+
+import com.calorie.instant.ui.CameraPreview;
+import com.calorie.instant.util.Log;
+
+/**
+ * Takes a photo saves it to the SD card and returns the path of this photo to the calling Activity
+ * @author paul.blundell
+ *
+ */
+public class CameraActivity extends Activity implements PictureCallback {
+
+	protected static final String EXTRA_IMAGE_PATH = "com.blundell.tut.cameraoverlay.ui.CameraActivity.EXTRA_IMAGE_PATH";
+
+	private Camera camera;
+	private CameraPreview cameraPreview;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_camera);
+		setResult(RESULT_CANCELED);
+		// Camera may be in use by another activity or the system or not available at all
+		camera = getCameraInstance();
+		if(cameraAvailable(camera)){
+			initCameraPreview();
+		} else {
+			finish();
+		}
+	}
+
+	// Show the camera view on the activity
+	private void initCameraPreview() {
+		cameraPreview = (CameraPreview) findViewById(R.id.camera_preview);
+		cameraPreview.init(camera);
+	}
+
+	
+	public void onCaptureClick(View button){
+		// Take a picture with a callback when the photo has been created
+		// Here you can add callbacks if you want to give feedback when the picture is being taken
+		camera.takePicture(null, null, this);
+	}
+
+	@Override
+	public void onPictureTaken(byte[] data, Camera camera) {
+		Log.d("Picture taken");
+		String path = savePictureToFileSystem(data);
+		setResult(path);
+		finish();
+	}
+
+	private static String savePictureToFileSystem(byte[] data) {
+		File file = getOutputMediaFile();
+		saveToFile(data, file);
+		return file.getAbsolutePath();
+	}
+
+	private void setResult(String path) {
+		Intent intent = new Intent();
+		intent.putExtra(EXTRA_IMAGE_PATH, path);
+		setResult(RESULT_OK, intent);
+	}
+
+	// ALWAYS remember to release the camera when you are finished
+	@Override
+	protected void onPause() {
+		super.onPause();
+		releaseCamera();
+	}
+
+	private void releaseCamera() {
+		if(camera != null){
+			camera.release();
+			camera = null;
+		}
+	}
+}
--- src/com/calorie/instant/MainActivity.java
+++ src/com/calorie/instant/MainActivity.java
@@ -1,17 +1,61 @@
 package com.calorie.instant;
 
+import com.calorie.instant.util.BitmapHelper;
+import com.calorie.instant.util.Log;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity
 {
+	private static final int REQ_CAMERA_IMAGE = 123;
 
 	@Override
-	protected void onCreate( Bundle savedInstanceState )
-	{
-		super.onCreate( savedInstanceState );
-		setContentView( R.layout.activity_main );
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+
+		String message = "Click the button below to start";
+		if(cameraNotDetected()){
+			message = "No camera detected, clicking the button below will have unexpected behaviour.";
+		}
+
+		TextView cameraDescriptionTextView = (TextView) findViewById(R.id.text_view_camera_description);
+		cameraDescriptionTextView.setText(message);
+	}
+
+	private boolean cameraNotDetected() {
+		return !getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+	}
+
+
+	public void onUseCameraClick(View button){
+		Intent intent = new Intent(this, CameraActivity.class);
+		startActivityForResult(intent, REQ_CAMERA_IMAGE);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(requestCode == REQ_CAMERA_IMAGE && resultCode == RESULT_OK){
+			String imgPath = data.getStringExtra(CameraActivity.EXTRA_IMAGE_PATH);
+			Log.i("Got image path: "+ imgPath);
+			displayImage(imgPath);
+		} else
+			if(requestCode == REQ_CAMERA_IMAGE && resultCode == RESULT_CANCELED){
+				Log.i("User didn't take an image");
+			}
+	}
+
+	private void displayImage(String path) {
+		ImageView imageView = (ImageView) findViewById(R.id.image_view_captured_image);
+		imageView.setImageBitmap(BitmapHelper.decodeSampledBitmap(path, 300, 250));
 	}
 
 	@Override
