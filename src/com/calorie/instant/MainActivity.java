8f5a483245f01e566026719ce9b4335a70e31da7
leebeth <lorena_2328@hotmail.com>
2015-11-02 23:04:35 -0500

    merge opencv parte 2

--- src/com/calorie/instant/MainActivity.java
+++ src/com/calorie/instant/MainActivity.java
@@ -1,20 +1,38 @@
 package com.calorie.instant;
 
-import com.calorie.instant.util.BitmapHelper;
-import com.calorie.instant.util.Log;
 
-import android.os.Bundle;
+import java.util.List;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.view.Menu;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.calorie.instant.util.BitmapHelper;
+import com.calorie.instant.util.Log;
+
 public class MainActivity extends Activity
 {
 	private static final int REQ_CAMERA_IMAGE = 123;
+
+	private static final String  TAG = "OCVSample::MainActivity";
+	
+	private ColorBlobDetector mDetector;
+	private boolean openCVLoaded;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -47,23 +65,85 @@ public class MainActivity extends Activity
 			String imgPath = data.getStringExtra(CameraActivity.EXTRA_IMAGE_PATH);
 			Log.i("Got image path: "+ imgPath);
 			displayImage(imgPath);
+			calcularArea(imgPath);
 		} else
 			if(requestCode == REQ_CAMERA_IMAGE && resultCode == RESULT_CANCELED){
 				Log.i("User didn't take an image");
 			}
 	}
 
+	private void calcularArea( String imgPath )
+	{
+		android.util.Log.i(TAG, "Entra a calcular area");
+		if(openCVLoaded)
+		{
+			android.util.Log.i(TAG, "Entra a calcular area y opencv esta cargado");
+			try
+			{
+				Bitmap bitmap = BitmapFactory.decodeResource( getResources(), R.raw.c300 );
+				Mat src = new Mat();
+				Utils.bitmapToMat( bitmap, src );
+				mDetector.process(src);
+				List<MatOfPoint> contours = mDetector.getContours();
+				
+				Moments[] mu = new Moments[contours.size( )];
+
+				/// Get the moments
+				for ( int i = 0; i < contours.size( ); i++ )
+				{
+					mu[i] = Imgproc.moments(contours.get( i ));
+				}
+
+				String areas = "";
+				for( int i = 0; i< contours.size(); i++ )
+				{
+					areas += mu[i].get_m00( ) + "\n";
+				}
+				
+				android.util.Log.i(TAG, areas);
+				
+			}
+			catch ( Exception e )
+			{
+				e.printStackTrace();
+			}
+		}
+		else
+		{
+			android.util.Log.e( TAG, "OpenCV no cargado" );
+		}
+	}
+
 	private void displayImage(String path) {
 		ImageView imageView = (ImageView) findViewById(R.id.image_view_captured_image);
 		imageView.setImageBitmap(BitmapHelper.decodeSampledBitmap(path, 300, 250));
 	}
+	
+	private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
+		
 
+		@Override
+		public void onManagerConnected(int status) {
+			switch (status) {
+			case LoaderCallbackInterface.SUCCESS:
+			{
+				android.util.Log.i(TAG, "OpenCV loaded successfully");
+				mDetector = new ColorBlobDetector();
+				openCVLoaded=true;				
+			} break;
+			default:
+			{
+				super.onManagerConnected(status);
+			} break;
+			}
+		}
+	};
+	
 	@Override
-	public boolean onCreateOptionsMenu( Menu menu )
+	public void onResume()
 	{
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater( ).inflate( R.menu.main, menu );
-		return true;
+		super.onResume();
+		OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_3, this, mLoaderCallback);
 	}
 
 }
