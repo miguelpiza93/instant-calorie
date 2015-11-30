38f75294b9e1746248a165af7a144e1900a88c17
leebeth <lorena_2328@hotmail.com>
2015-11-29 19:26:18 -0500

    COMMMIT PRUEBA

--- src/com/calorie/instant/MainActivity.java
+++ src/com/calorie/instant/MainActivity.java
@@ -9,7 +9,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
